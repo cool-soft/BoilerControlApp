d530b527f0718d0ef826ee62e1e244f03d3c8b9c
Georgij <netbay.georgij@gmail.com>
2020-11-16 02:33:01 +0500

    forecasted weather t table is added to database

--- src/create_statistic_db.py
+++ src/create_statistic_db.py
@@ -9,15 +9,22 @@ if __name__ == '__main__':
     conn = sqlite3.connect(config.DB_PATH)
     cursor = conn.cursor()
 
-    cursor.execute(
+    cursor.executescript(
         """
-        CREATE TABLE control_boiler_t
-            (
-                boiler_id UNSIGNED, 
-                boiler_subid UNSIGNED,
-                boiler_t UNSIGNED,
-                timestamp DATETIME
-            )
+        CREATE TABLE control_boiler_t 
+        (
+            boiler_id UNSIGNED, 
+            boiler_subid UNSIGNED,
+            boiler_t UNSIGNED,
+            timestamp TEXT
+        );
+        
+        CREATE TABLE forecasted_weather_t
+        (
+            city TEXT,
+            weather_t UNSIGNED,
+            timestamp TEXT
+        );
         """
     )
     conn.commit()
