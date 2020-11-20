6a84cbd685bf63a0c51acb76b5a5df574e5c6264
Georgij <netbay.georgij@gmail.com>
2020-11-21 04:43:31 +0500

    ~~~

--- src/datasets_utils/config.py
+++ src/datasets_utils/config.py
@@ -2,7 +2,6 @@
 HOME_T_DISPERSION_COEFFICIENT = 0.97
 
 REMOTE_HOST = "https://lysva.agt.town/"
-BOILER_ID = 1
 
 T_GRAPH_PATH = "storage\\t_graph.csv"
 HOMES_DELTAS_PATH = "storage\\homes_time_delta.csv"
