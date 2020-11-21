e609b6c9e9ec6ae58702aaeb7ca3c59eec72104b
Georgij <netbay.georgij@gmail.com>
2020-11-22 01:49:45 +0500

    Refactoring

--- src/boiler_t_prediction/boiler_t_predictor.py
+++ src/boiler_t_prediction/boiler_t_predictor.py
@@ -32,30 +32,16 @@ class BoilerTPredictor:
     def get_boiler_t(self, start_datetime, end_datetime):
         max_home_time_delta = self._homes_time_deltas[consts.TIME_DELTA_COLUMN_NAME].max()
 
-        t_graph_requirements_end_datetime = end_datetime + (max_home_time_delta * consts.TIME_TICK)
-        t_graph_requirements_df = self._get_t_graph_requirements(start_datetime, t_graph_requirements_end_datetime)
+        weather_forecast_end_datetime = end_datetime + (max_home_time_delta * consts.TIME_TICK)
+        weather_forecast_df = self._weather_forecast_provider.get_weather_forecast(
+            start_datetime, weather_forecast_end_datetime
+        )
+        t_graph_requirements_df = self._get_t_graph_requirements(weather_forecast_df)
+        need_boiler_t_df = self._get_need_boiler_t(t_graph_requirements_df)
 
-        predicted_boiler_t_count = len(t_graph_requirements_df) - max_home_time_delta
+        return need_boiler_t_df
 
-        t_graph_requirements_arr = t_graph_requirements_df[consts.HOME_T_COLUMN_NAME].to_numpy()
-        predicted_boiler_t_arr = np.empty(shape=(predicted_boiler_t_count,), dtype=np.float)
-        for idx in range(predicted_boiler_t_count):
-            need_t_by_homes = self._get_need_t_in_homes(idx, t_graph_requirements_arr)
-            need_boiler_t = self._calc_need_boiler_t_by_homes_t(need_t_by_homes)
-            predicted_boiler_t_arr[idx] = need_boiler_t
-
-        t_graph_requirements_datetime_list = t_graph_requirements_df[consts.TIMESTAMP_COLUMN_NAME].to_list()
-        predicted_boiler_t_dates = t_graph_requirements_datetime_list[:predicted_boiler_t_count]
-
-        predicted_boiler_t_df = pd.DataFrame({
-            consts.BOILER_NAME_COLUMN_NAME: predicted_boiler_t_arr,
-            consts.TIMESTAMP_COLUMN_NAME: predicted_boiler_t_dates
-        })
-
-        return predicted_boiler_t_df
-
-    def _get_t_graph_requirements(self, start_datetime, end_datetime):
-        weather_forecast_df = self._weather_forecast_provider.get_weather_forecast(start_datetime, end_datetime)
+    def _get_t_graph_requirements(self, weather_forecast_df):
         weather_t_forecast_arr = weather_forecast_df[consts.WEATHER_T_COLUMN_NAME].to_numpy()
 
         weather_t_forecast_len = len(weather_t_forecast_arr)
@@ -79,18 +65,31 @@ class BoilerTPredictor:
         need_t_in_home_by_t_graph = available_t[consts.HOME_T_COLUMN_NAME].min()
         return need_t_in_home_by_t_graph
 
-    def _get_need_t_in_homes(self, t_idx, t_graph_requirements_arr):
-        need_temps = {}
-        for index, row in self._homes_time_deltas.iterrows():
-            home_time_delta = row[consts.TIME_DELTA_COLUMN_NAME]
-            home_name = row[consts.HOME_NAME_COLUMN_NAME]
+    def _get_need_boiler_t(self, t_graph_requirements_df):
+        max_home_time_delta = self._homes_time_deltas[consts.TIME_DELTA_COLUMN_NAME].max()
+        need_boiler_t_df_len = len(t_graph_requirements_df) - max_home_time_delta
 
-            need_t = t_graph_requirements_arr[t_idx + home_time_delta]
-            need_temps[home_name] = need_t
+        t_graph_requirements_arr = t_graph_requirements_df[consts.HOME_T_COLUMN_NAME].to_numpy()
+        need_boiler_t_arr = np.empty(shape=(need_boiler_t_df_len,), dtype=np.float)
+        for i in range(need_boiler_t_df_len):
+            need_t_in_homes = {}
+            for index, row in self._homes_time_deltas.iterrows():
+                home_time_delta = row[consts.TIME_DELTA_COLUMN_NAME]
+                home_name = row[consts.HOME_NAME_COLUMN_NAME]
+                need_t = t_graph_requirements_arr[i + home_time_delta]
+                need_t_in_homes[home_name] = need_t
+            need_boiler_t = self._calc_boiler_t_by_need_homes_t(need_t_in_homes)
+            need_boiler_t_arr[i] = need_boiler_t
 
-        return need_temps
+        t_graph_requirements_dates_list = t_graph_requirements_df[consts.TIMESTAMP_COLUMN_NAME].to_list()
+        need_boiler_t_dates = t_graph_requirements_dates_list[:need_boiler_t_df_len]
+        need_boiler_t_df = pd.DataFrame({
+            consts.TIMESTAMP_COLUMN_NAME: need_boiler_t_dates,
+            consts.BOILER_NAME_COLUMN_NAME: need_boiler_t_arr
+        })
+        return need_boiler_t_df
 
-    def _calc_need_boiler_t_by_homes_t(self, need_t_by_homes):
+    def _calc_boiler_t_by_need_homes_t(self, need_t_by_homes):
         iterator = iter(need_t_by_homes.items())
         home_name, need_home_t = next(iterator)
         need_t_condition = self._optimized_t_table[home_name] >= need_home_t
