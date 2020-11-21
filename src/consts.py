b5e3186f9481c4f2ae61cc44b80b7e9aae34dfc8
Georgij <netbay.georgij@gmail.com>
2020-11-21 21:47:05 +0500

    TIME_STEP is returned to 3 minutes

--- src/consts.py
+++ src/consts.py
@@ -1,7 +1,7 @@
 
 import datetime
 
-TIME_STEP = datetime.timedelta(minutes=2)
+TIME_STEP = datetime.timedelta(minutes=3)
 
 TIMESTAMP_COLUMN_NAME = "dTimeStamp"
 WEATHER_T_COLUMN_NAME = "weather_t"
