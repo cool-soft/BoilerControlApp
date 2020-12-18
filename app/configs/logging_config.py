c5d49a651da51190c2b3c9b92543151892ff6cf5
Georgij <netbay.georgij@gmail.com>
2020-12-18 22:13:28 +0500

    ~~~

--- app/configs/logging_config.py
+++ app/configs/logging_config.py
@@ -1,9 +1,11 @@
+import logging
+
 from pydantic import BaseModel
 
 
 class LoggingConfig(BaseModel):
     path: str = "../logs/log.log"
-    level: str = "DEBUG"
+    level: str = logging.DEBUG
     datetime_format: str = "%Y-%m-%d %H:%M:%S"
     # noinspection SpellCheckingInspection
     format: str = "[%(asctime)s] %(levelname)s - %(message)s"
