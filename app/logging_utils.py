7941ecd94d2d72200753aa7e20b82584baf17b53
Georgij <netbay.georgij@gmail.com>
2020-12-18 22:30:27 +0500

    Logging to console is added. Logging file rotation is added

---
+++ app/logging_utils.py
@@ -0,0 +1,24 @@
+import logging.handlers
+import sys
+
+from configs.logging_config import LoggingConfig
+
+
+def setup_default_logger_from_config(config: LoggingConfig):
+    logging_handlers = (
+        logging.handlers.RotatingFileHandler(
+            filename=config.path,
+            maxBytes=config.max_bytes,
+            backupCount=config.backup_count
+        ),
+        logging.StreamHandler(
+            stream=sys.stderr
+        )
+    )
+    # noinspection PyArgumentList
+    logging.basicConfig(
+        level=config.level,
+        datefmt=config.datetime_format,
+        format=config.format,
+        handlers=logging_handlers
+    )
