710d30a53ad129113cd90132b9865b31073cf4bd
Georgij <netbay.georgij@gmail.com>
2020-12-18 21:55:11 +0500

    Autogeneration of config file is added

--- app/configs/boiler_t_predictor_config.py
+++ app/configs/boiler_t_predictor_config.py
@@ -5,7 +5,7 @@ from configs.weather_forecast_provider_config import WeatherForecastProviderConf
 
 class BoilerTPredictorConfig(BaseModel):
     home_t_dispersion_coefficient: float = 0.97
-    t_graph_path: str = "../../storage/t_graph.csv"
-    homes_deltas_path: str = "../../storage/homes_time_delta.csv"
-    optimized_t_table_path: str = "../../storage/optimized_t_table.pickle"
+    t_graph_path: str = "../storage/t_graph.csv"
+    homes_deltas_path: str = "../storage/homes_time_delta.csv"
+    optimized_t_table_path: str = "../storage/optimized_t_table.pickle"
     weather_forecast_provider: WeatherForecastProviderConfig = WeatherForecastProviderConfig()
--- app/configs/logging_config.py
+++ app/configs/logging_config.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 
 
 class LoggingConfig(BaseModel):
-    path: str = "../../logs/log.log"
+    path: str = "../logs/log.log"
     level: str = "DEBUG"
     datetime_format: str = "%Y-%m-%d %H:%M:%S"
     # noinspection SpellCheckingInspection
