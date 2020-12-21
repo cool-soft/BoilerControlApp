ac75989f6649479f8c452403f256d532e150c1a7
Georgij <netbay.georgij@gmail.com>
2020-12-22 03:29:36 +0500

    ~~~

--- app/endpoints/api_v1.py
+++ app/endpoints/api_v1.py
@@ -19,8 +19,9 @@ api_router = APIRouter(prefix="/api/v1")
 @inject
 def get_predicted_boiler_t(
         dates_range: InputDatesRange = Depends(),
-        timezone_name: Optional[str] = Depends(Provide[Core.config.datetime_processing.boiler_controller_timezone]),
-        boiler_t_predictor: SimpleBoilerTPredictorService = Depends(Provide[Services.boiler_t_predictor_service])
+        timezone_name: Optional[str] = None,
+        boiler_t_predictor: SimpleBoilerTPredictorService = Depends(Provide[Services.boiler_t_predictor_service]),
+        datetime_processing_params=Depends(Provide[Core.config.datetime_processing])
 ):
     """
     Метод для получения рекомендуемой температуры, которую необходимо выставить на бойлере.
@@ -34,14 +35,18 @@ def get_predicted_boiler_t(
                   f"from {dates_range.start_date} to {dates_range.end_date}"
                   f"with timezone_name {timezone_name}")
 
+    if timezone_name is None:
+        timezone_name = datetime_processing_params.get("default_timezone")
+
     predicted_boiler_t_df = boiler_t_predictor.get_need_boiler_t(dates_range.start_date, dates_range.end_date)
 
-    boiler_control_timezone = gettz(timezone_name)
+    work_timezone = gettz(timezone_name)
+    response_datetime_pattern = datetime_processing_params.get("response_pattern")
     predicted_boiler_t_ds = []
     for _, row in predicted_boiler_t_df.iterrows():
         datetime_ = row[data_consts.TIMESTAMP_COLUMN_NAME]
-        datetime_ = datetime_.astimezone(boiler_control_timezone)
-        datetime_as_str = datetime_.strftime('%Y-%m-%d %H:%M')
+        datetime_ = datetime_.astimezone(work_timezone)
+        datetime_as_str = datetime_.strftime(response_datetime_pattern)
 
         boiler_t = row[data_consts.BOILER_NAME_COLUMN_NAME]
         boiler_t = round(boiler_t, 1)
--- app/endpoints/dependencies.py
+++ app/endpoints/dependencies.py
@@ -2,36 +2,36 @@ from datetime import datetime
 from typing import Optional
 
 from dateutil.tz import gettz
+from dependency_injector.wiring import inject, Provide
+from fastapi import Depends
 
+from containers.core import Core
 from dataset_utils import data_consts
 from dataset_utils.preprocess_utils import parse_datetime
 
 
 class InputDatesRange:
 
-    def __init__(self, start_date: Optional[str] = None, end_date: Optional[str] = None):
-        boiler_control_timezone = gettz("Asia/Yekaterinburg")
+    @inject
+    def __init__(
+            self,
+            start_date: Optional[str] = None,
+            end_date: Optional[str] = None,
+            timezone_name: Optional[str] = None,
+            datetime_processing_params=Depends(Provide[Core.config.datetime_processing])
+    ):
+        if timezone_name is None:
+            timezone_name = datetime_processing_params.get("default_timezone")
+
+        work_timezone = gettz(timezone_name)
+        request_datetime_patterns = datetime_processing_params.get("request_patterns")
 
         if start_date is None:
-            self.start_date = datetime.now(tz=boiler_control_timezone)
+            self.start_date = datetime.now(tz=work_timezone)
         else:
-            self.start_date = parse_datetime(
-                start_date,
-                (
-                    r'(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})\s(?P<hours>\d{1,2}):(?P<minutes>\d{2})',
-                    r'(?P<day>\d{1,2})\.(?P<month>\d{1,2})\.(?P<year>\d{4})\s(?P<hours>\d{1,2}):(?P<minutes>\d{2})'
-                ),
-                timezone=boiler_control_timezone
-            )
+            self.start_date = parse_datetime(start_date, request_datetime_patterns, timezone=work_timezone)
 
         if end_date is None:
             self.end_date = self.start_date + data_consts.TIME_TICK
         else:
-            self.end_date = parse_datetime(
-                end_date,
-                (
-                    r'(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})\s(?P<hours>\d{1,2}):(?P<minutes>\d{2})',
-                    r'(?P<day>\d{1,2})\.(?P<month>\d{1,2})\.(?P<year>\d{4})\s(?P<hours>\d{1,2}):(?P<minutes>\d{2})'
-                ),
-                timezone=boiler_control_timezone
-            )
+            self.end_date = parse_datetime(end_date, request_datetime_patterns, timezone=work_timezone)
