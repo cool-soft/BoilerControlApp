1a3cf57a6815d5654e1c7978f408493f0d42aba4
Georgij <netbay.georgij@gmail.com>
2020-12-22 03:32:24 +0500

    ~~~

--- app/containers/server.py
+++ app/containers/server.py
@@ -8,14 +8,14 @@ class Server(containers.DeclarativeContainer):
     config = providers.Configuration()
 
     fast_api_app = providers.Singleton(fastapi.FastAPI)
-    server_config = providers.Singleton(
-        UvicornConfig,
-        app=fast_api_app,
-        host=config.host,
-        port=config.port,
-        log_config=None
-    )
+    
     server = providers.Singleton(
         UvicornServer,
-        config=server_config
+        config=providers.Singleton(
+            UvicornConfig,
+            app=fast_api_app,
+            host=config.host,
+            port=config.port,
+            log_config=None
+        )
     )
