b556681c74be982fd284e9524f56a5a6d5d80220
Georgij <netbay.georgij@gmail.com>
2020-12-23 00:16:15 +0500

    Logging is added for resources loading

--- app/resources/home_time_deltas_resource.py
+++ app/resources/home_time_deltas_resource.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import pandas as pd
@@ -7,7 +8,9 @@ from dependency_injector import resources
 class HomeTimeDeltasResource(resources.Resource):
 
     def init(self, homes_deltas_path) -> pd.DataFrame:
+        logger = logging.getLogger(self.__class__.__name__)
         homes_deltas_path = os.path.abspath(homes_deltas_path)
+        logger.debug(f"Loading home time deltas from {homes_deltas_path}")
         homes_time_deltas = pd.read_csv(homes_deltas_path)
         return homes_time_deltas
 
--- app/resources/temp_graph_resource.py
+++ app/resources/temp_graph_resource.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from dependency_injector import resources
@@ -7,9 +8,11 @@ import pandas as pd
 class TempGraphResource(resources.Resource):
 
     def init(self, temp_graph_path) -> pd.DataFrame:
+        logger = logging.getLogger(self.__class__.__name__)
         temp_graph_path = os.path.abspath(temp_graph_path)
-        optimized_t_table = pd.read_csv(temp_graph_path)
-        return optimized_t_table
+        logger.debug(f"Loading temp graph from {temp_graph_path}")
+        temp_graph = pd.read_csv(temp_graph_path)
+        return temp_graph
 
-    def shutdown(self, optimized_t_table: pd.DataFrame):
+    def shutdown(self, temp_graph: pd.DataFrame):
         pass
