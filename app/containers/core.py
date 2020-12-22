ede435da738663e471884545b5ebe5f14d3b32d9
Georgij <netbay.georgij@gmail.com>
2020-12-22 23:16:48 +0500

    Logging is added

--- app/containers/core.py
+++ app/containers/core.py
@@ -1,5 +1,11 @@
 from dependency_injector import containers, providers
+import logging.config
 
 
 class Core(containers.DeclarativeContainer):
     config = providers.Configuration()
+
+    logging = providers.Resource(
+        logging.config.dictConfig,
+        config=config.logging,
+    )
