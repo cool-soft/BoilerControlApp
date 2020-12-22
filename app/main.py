84bba0d25442fdf7b8f1703a19820dc1478e8310
Georgij <netbay.georgij@gmail.com>
2020-12-22 22:52:40 +0500

    app creation is moved from Server Container

--- app/main.py
+++ app/main.py
@@ -1,16 +1,22 @@
+from dependency_injector import providers
+from fastapi import FastAPI
+
 from containers.application import Application
 from endpoints import api_v1, api_v2
 
 if __name__ == '__main__':
     application = Application()
-    application.config.from_yaml('../config.yaml')
-    application.init_resources()
-    application.core.wire(modules=(api_v1, api_v2))
-    application.services.wire(modules=(api_v1, api_v2))
 
-    app = application.server.fast_api_app()
+    app = FastAPI()
     app.include_router(api_v1.api_router)
     app.include_router(api_v2.api_router)
+    application.server.app.override(providers.Object(app))
+
+    application.config.from_yaml('../config.yaml')
+    application.init_resources()
+    
+    application.core.wire(modules=(api_v1, api_v2))
+    application.services.wire(modules=(api_v1, api_v2))
 
     server = application.server.server()
     server.run()
