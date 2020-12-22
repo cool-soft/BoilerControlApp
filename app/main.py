4450caf5c75a17b43acf285e5389bb5cdccb7521
Georgij <netbay.georgij@gmail.com>
2020-12-23 00:19:14 +0500

    Logging is added for __main__ module

--- app/main.py
+++ app/main.py
@@ -10,20 +10,23 @@ if __name__ == '__main__':
     CONFIG_PATH = "../config.yaml"
 
     application = Application()
+    application.config.from_yaml(CONFIG_PATH)
 
+    application.core.init_resources()
+    application.services.init_resources()
+
+    logger = logging.getLogger(__name__)  # Must be placed after core.init_resources()
+
+    logger.debug("Wiring")
+    application.core.wire(modules=(api_v1, api_v2))
+    application.services.wire(modules=(api_v1, api_v2))
+
+    logger.debug("Creating FastAPI app")
     app = FastAPI()
     app.include_router(api_v1.api_router)
     app.include_router(api_v2.api_router)
     application.server.app.override(providers.Object(app))
 
-    application.config.from_yaml(CONFIG_PATH)
-    application.core.init_resources()
-    application.services.init_resources()
-
-    application.core.wire(modules=(api_v1, api_v2))
-    application.services.wire(modules=(api_v1, api_v2))
-
     server = application.server.server()
-    logger = logging.getLogger(__name__)
     logger.debug(f"Starting server at {server.config.host}:{server.config.port}")
     server.run()
