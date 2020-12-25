7b9491e489e0f04c687049dc79979dea4f1d1dfd
Georgij <netbay.georgij@gmail.com>
2020-12-25 10:38:44 +0500

    Unused test is deleted

--- tests/test_yaml_config.py
+++
@@ -1,8 +0,0 @@
-from configs.app_config import GlobalAppConfig
-
-if __name__ == '__main__':
-    cfg1 = GlobalAppConfig()
-    cfg2 = GlobalAppConfig.load_app_config("../config.yaml")
-    print(cfg1 is cfg2)
-    cfg1.logging.level = "INFO"
-    print(cfg2.logging.level == "INFO")
