841728f797014885a3c20e7318654d3f39696096
Georgij <netbay.georgij@gmail.com>
2020-12-27 00:37:15 +0500

    ~~~

--- app/main.py
+++ app/main.py
@@ -6,25 +6,39 @@ from fastapi import FastAPI
 from containers.application import Application
 from endpoints import api_v1, api_v2
 
+
+def create_fast_api_app():
+    fast_api_app = FastAPI()
+    fast_api_app.include_router(api_v1.api_router)
+    fast_api_app.include_router(api_v2.api_router)
+    return fast_api_app
+
+
+def wire(application_container):
+    application_container.core.wire(modules=(api_v1, api_v2))
+    application_container.services.wire(modules=(api_v1, api_v2))
+
+
+def init_resources(application_container):
+    application_container.core.init_resources()
+    application_container.services.boiler_t_prediction.init_resources()
+
+
 if __name__ == '__main__':
     CONFIG_PATH = "../configs/config.yaml"
 
     application = Application()
     application.config.from_yaml(CONFIG_PATH)
 
-    application.core.init_resources()
-    application.services.boiler_t_prediction.init_resources()
+    init_resources(application)
 
     logger = logging.getLogger(__name__)  # Must be placed after core.init_resources()
 
     logger.debug("Wiring")
-    application.core.wire(modules=(api_v1, api_v2))
-    application.services.wire(modules=(api_v1, api_v2))
+    wire(application)
 
     logger.debug("Creating FastAPI app")
-    app = FastAPI()
-    app.include_router(api_v1.api_router)
-    app.include_router(api_v2.api_router)
+    app = create_fast_api_app()
     application.server.app.override(providers.Object(app))
 
     server = application.server.server()
