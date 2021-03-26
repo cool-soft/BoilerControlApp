b0448c926b7e4f5bbc5d691f4ad2eb33757af8f3
Georgij <netbay.georgij@gmail.com>
2021-03-26 22:40:27 +0500

    Добавляем сообщения об ошибках при парсинге: неверный диапазон/таймзона

--- backend/endpoints/dependencies.py
+++ backend/endpoints/dependencies.py
@@ -1,15 +1,15 @@
 from datetime import datetime
-
-import pandas as pd
 from typing import Optional
 
+import pandas as pd
 from dateutil.tz import gettz
 from dependency_injector.wiring import inject, Provide
-from fastapi import Depends
+from fastapi import Depends, HTTPException
+from starlette import status
 
+from backend.containers.core import Core
 from boiler.constants import time_tick
 from boiler.parsing_utils.datetime_parsing import parse_datetime
-from backend.containers.core import Core
 
 
 class InputTimezone:
@@ -22,8 +22,16 @@ class InputTimezone:
     ):
         if timezone_name is None:
             timezone_name = datetime_processing_params.get("default_timezone")
+
+        timezone = gettz(timezone_name)
+        if timezone is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"incorrect timezone_name \"{timezone_name}\"",
+            )
+
+        self.timezone = timezone
         self.name = timezone_name
-        self.timezone = gettz(timezone_name)
 
 
 class InputDatesRange:
@@ -44,7 +52,6 @@ class InputDatesRange:
             start_date = pd.Timestamp(
                 parse_datetime(start_date, request_datetime_patterns, timezone=work_timezone.timezone)
             )
-        self.start_date = start_date
 
         if end_date is None:
             end_date = start_date + time_tick.TIME_TICK
@@ -52,6 +59,14 @@ class InputDatesRange:
             end_date = pd.Timestamp(
                 parse_datetime(end_date, request_datetime_patterns, timezone=work_timezone.timezone)
             )
+
+        if start_date <= end_date:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="start_date must be less than end_date"
+            )
+
+        self.start_date = start_date
         self.end_date = end_date
 
 
@@ -70,7 +85,6 @@ class InputDatetimeRange:
             start_datetime = pd.Timestamp(start_datetime)
         if start_datetime.tz is None:
             start_datetime = start_datetime.tz_localize(tz=work_timezone.timezone)
-        self.start_datetime = start_datetime
 
         if end_datetime is None:
             end_datetime = start_datetime + time_tick.TIME_TICK
@@ -78,4 +92,12 @@ class InputDatetimeRange:
             end_datetime = pd.Timestamp(end_datetime)
         if end_datetime.tz is None:
             end_datetime = end_datetime.tz_localize(tz=work_timezone.timezone)
+
+        if start_datetime <= end_datetime:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="start_datetime must be less than end_datetime"
+            )
+
+        self.start_datetime = start_datetime
         self.end_datetime = end_datetime
