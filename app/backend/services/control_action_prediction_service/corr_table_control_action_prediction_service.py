1b6d056e64200206b7542253e46f6dab06c7fa92
Georgij <netbay.georgij@gmail.com>
2021-04-03 22:50:03 +0500

    ~~~

--- app/backend/services/control_action_prediction_service/corr_table_control_action_prediction_service.py
+++ app/backend/services/control_action_prediction_service/corr_table_control_action_prediction_service.py
@@ -4,12 +4,12 @@ import logging
 import pandas as pd
 from dateutil.tz import tzlocal
 
-from boiler.constants import column_names, time_tick
-from boiler.temp_predictors.corr_table_temp_predictor import CorrTableTempPredictor
 from backend.repositories.control_action_cache_repository import ControlActionsCacheRepository
 from backend.repositories.temp_requirements_cache_repository import TempRequirementsCacheRepository
 from backend.services.control_action_prediction_service.control_action_prediction_service \
     import ControlActionPredictionService
+from boiler.constants import column_names
+from boiler.temp_predictors.corr_table_temp_predictor import CorrTableTempPredictor
 
 
 class CorrTableControlActionPredictionService(ControlActionPredictionService):
@@ -39,11 +39,13 @@ class CorrTableControlActionPredictionService(ControlActionPredictionService):
         self._temp_predictor = temp_predictor
 
     async def update_control_actions(self):
-        self._logger.debug(f"Requested updating control actions")
+        self._logger.debug("Requested updating control actions")
 
-        with self._service_lock:
+        async with self._service_lock:
             start_datetime = pd.Timestamp.now(tz=tzlocal())
+            self._logger.debug(f"Requesting temp requirements from {start_datetime}")
             temp_requirements_df = await self._temp_requirements_repository.get_temp_requirements(start_datetime)
+            self._logger.debug(f"Gathered {len(temp_requirements_df)} temp requirements")
 
             required_temp_arr = temp_requirements_df[column_names.FORWARD_PIPE_COOLANT_TEMP].to_numpy()
             need_temp_list = self._temp_predictor.predict_on_temp_requirements(required_temp_arr)
