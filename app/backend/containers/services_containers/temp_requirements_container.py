0ec3b70464a878a1b701b6568abe5b9f530bd562
Georgij <netbay.georgij@gmail.com>
2021-04-25 18:28:49 +0500

    Переход на read/writer/dumper/loader

--- app/backend/containers/services_containers/temp_requirements_container.py
+++ app/backend/containers/services_containers/temp_requirements_container.py
@@ -1,13 +1,14 @@
 from dateutil.tz import gettz
 from dependency_injector import containers, providers
 
-from boiler.temp_requirements.calculators.temp_graph_requirements_calculator import TempGraphRequirementsCalculator
-from boiler.weater_info.interpolators.weather_data_linear_interpolator import WeatherDataLinearInterpolator
+from boiler.temp_requirements.calculators.temp_graph_requirements_calculator \
+    import TempGraphRequirementsCalculator
 from boiler.temp_requirements.repository.db.async_.temp_requirements_db_async_fake_repository \
     import TempRequirementsDBAsyncFakeRepository
-from boiler_softm.weater_info.parsers.soft_m_json_weather_data_parser import SoftMJSONWeatherDataParser
-from boiler_softm.weater_info.repository.stream.async_.soft_m_online_weather_forecast_stream_async_repository \
-    import SoftMOnlineWeatherForecastStreamAsyncRepository
+from boiler_softm.weather.io.sync.soft_m_sync_weather_forecast_json_reader \
+    import SoftMSyncWeatherForecastJSONReader
+from boiler_softm.weather.io.async_.soft_m_async_weather_forecast_online_loader \
+    import SoftMAsyncWeatherForecastOnlineLoader
 from backend.services.temp_requirements_update_service.simple_temp_requirements_service \
     import SimpleTempRequirementsService
 
@@ -15,22 +16,20 @@ from backend.services.temp_requirements_update_service.simple_temp_requirements_
 class TempRequirementsContainer(containers.DeclarativeContainer):
     config = providers.Configuration()
 
-    temp_graph_repository = providers.Dependency()
+    temp_graph_loader = providers.Dependency()
 
     temp_requirements_calculator = providers.Singleton(TempGraphRequirementsCalculator)
 
-    weather_data_timezone = providers.Callable(gettz, config.weather_server_timezone)
-    weather_data_parser = providers.Singleton(SoftMJSONWeatherDataParser,
-                                              weather_data_timezone=weather_data_timezone)
-    weather_data_interpolator = providers.Singleton(WeatherDataLinearInterpolator)
-    weather_repository = providers.Singleton(SoftMOnlineWeatherForecastStreamAsyncRepository,
-                                             weather_data_parser=weather_data_parser,
-                                             weather_data_interpolator=weather_data_interpolator)
+    weather_forecast_timezone = providers.Callable(gettz, config.weather_server_timezone)
+    weather_forecast_reader = providers.Singleton(SoftMSyncWeatherForecastJSONReader,
+                                                  weather_data_timezone=weather_forecast_timezone)
+    weather_forecast_loader = providers.Singleton(SoftMAsyncWeatherForecastOnlineLoader,
+                                                  weather_reader=weather_forecast_reader)
 
     temp_requirements_repository = providers.Singleton(TempRequirementsDBAsyncFakeRepository)
 
     temp_requirements_service = providers.Singleton(SimpleTempRequirementsService,
-                                                    temp_graph_repository=temp_graph_repository,
-                                                    weather_repository=weather_repository,
+                                                    temp_graph_loader=temp_graph_loader,
+                                                    weather_loader=weather_forecast_loader,
                                                     temp_requirements_repository=temp_requirements_repository,
                                                     temp_graph_requirements_calculator=temp_requirements_calculator)
--- app/backend/services/temp_graph_update_service/simple_temp_graph_update_service.py
+++ app/backend/services/temp_graph_update_service/simple_temp_graph_update_service.py
@@ -1,39 +1,38 @@
 import asyncio
 import logging
+from typing import Optional
 
 from backend.services.temp_graph_update_service.temp_graph_update_service import TempGraphUpdateService
-from boiler.temp_graph.repository.stream.async_.temp_graph_stream_async_repository \
-    import TempGraphStreamAsyncRepository
+from boiler.temp_graph.io.async_.async_temp_graph_loader import AsyncTempGraphLoader
+from boiler.temp_graph.io.sync.sync_temp_graph_dumper import SyncTempGraphDumper
 
 
 class SimpleTempGraphUpdateService(TempGraphUpdateService):
 
     def __init__(self,
-                 temp_graph_src_repository: TempGraphStreamAsyncRepository = None,
-                 temp_graph_repository: TempGraphStreamAsyncRepository = None) -> None:
+                 temp_graph_loader: Optional[AsyncTempGraphLoader] = None,
+                 temp_graph_dumper: Optional[SyncTempGraphDumper] = None) -> None:
 
         self._logger = logging.getLogger(self.__class__.__name__)
-        self._logger.debug("Creating instance of the provider")
+        self._logger.debug("Creating instance")
 
         self._service_lock = asyncio.Lock()
 
-        self._temp_graph_src_repository = temp_graph_src_repository
-        self._temp_graph_repository = temp_graph_repository
+        self._temp_graph_loader = temp_graph_loader
+        self._temp_graph_dumper = temp_graph_dumper
 
-    def set_temp_graph_src_repository(self,
-                                      temp_graph_src_repository: TempGraphStreamAsyncRepository) -> None:
+    def set_temp_graph_loader(self,
+                              temp_graph_loader: AsyncTempGraphLoader) -> None:
         self._logger.debug("Temp graph src repository is set")
-        self._temp_graph_src_repository = temp_graph_src_repository
+        self._temp_graph_loader = temp_graph_loader
 
-    def set_temp_graph_repository(self, temp_graph_repository: TempGraphStreamAsyncRepository) -> None:
+    def set_temp_graph_dumper(self, temp_graph_dumper: SyncTempGraphDumper) -> None:
         self._logger.debug("Temp graph src repository is set")
-        self._temp_graph_repository = temp_graph_repository
+        self._temp_graph_dumper = temp_graph_dumper
 
     async def update_temp_graph_async(self):
         self._logger.debug("Requested temp graph update")
-
         async with self._service_lock:
-            temp_graph = await self._temp_graph_src_repository.get_temp_graph()
-            await self._temp_graph_repository.set_temp_graph(temp_graph)
-
+            temp_graph = await self._temp_graph_loader.load_temp_graph()
+            self._temp_graph_dumper.dump_temp_graph(temp_graph)
             self._logger.debug("temp graph is updated")
