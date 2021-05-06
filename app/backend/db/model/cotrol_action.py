181ca048343b9a49ebe13b162baa76ce124dc68a
Georgij <netbay.georgij@gmail.com>
2021-05-06 20:34:41 +0500

    ~~~

--- app/backend/db/model/cotrol_action.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, TIMESTAMP, BIGINT, SMALLINT
-
-from .base_model import BaseModel
-
-
-class ControlAction(BaseModel):
-    id = Column(BIGINT(), primary_key=True, unique=True, autoincrement=True)
-    timestamp = Column(TIMESTAMP(timezone=True), unique=True)
-    forward_temp = Column(SMALLINT())
