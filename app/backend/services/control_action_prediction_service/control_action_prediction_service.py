11cfd3920f230e7e453c33f2e5034e634dbc84d2
Georgij <netbay.georgij@gmail.com>
2021-05-20 21:49:15 +0500

    ~~~

--- app/backend/services/control_action_prediction_service/control_action_prediction_service.py
+++ app/backend/services/control_action_prediction_service/control_action_prediction_service.py
@@ -1,92 +1,86 @@
 import asyncio
 import logging
+from concurrent.futures.thread import ThreadPoolExecutor
+from typing import Tuple
 
 import pandas as pd
+from boiler.constants import dataset_prototypes
+from boiler.control_action.predictors.abstract_control_action_predictor import AbstractControlActionPredictor
+from boiler.data_processing.timestamp_round_algorithm import AbstractTimestampRoundAlgorithm
+from boiler.heating_system.model_requirements.abstract_model_requirements import AbstractModelRequirements
 from dateutil.tz import tzlocal
 
-from boiler.constants import column_names
-from backend.repositories.weather_forecast_repository import WeatherForecastRepository
-from boiler.heating_system_model.corr_table_heating_system_model import CorrTableHeatingSystemModel
 from backend.repositories.control_action_repository import ControlActionsRepository
-
+from backend.repositories.weather_forecast_repository import WeatherForecastRepository
 
 
 class ControlActionPredictionService:
 
     def __init__(self,
-                 temp_predictor: CorrTableTempPredictor = None,
-                 temp_requirements_repository: TempRequirementsDBAsyncRepository = None,
-                 control_actions_repository: ControlActionsSimpleRepository = None):
+                 weather_forecast_repository: WeatherForecastRepository,
+                 control_actions_repository: ControlActionsRepository,
+                 control_action_predictor: AbstractControlActionPredictor,
+                 model_requirements: AbstractModelRequirements,
+                 timestamp_round_algo: AbstractTimestampRoundAlgorithm,
+                 timedelta: pd.Timedelta,
+                 timedelta_predict_forward: pd.Timedelta = pd.Timedelta(seconds=3600),
+                 executor: ThreadPoolExecutor = None
+                 ) -> None:
         self._logger = logging.getLogger(self.__class__.__name__)
-        self._logger.debug("Creating instance of the provider")
+        self._logger.debug("Creating instance")
 
-        self._service_lock = asyncio.Lock()
-        self._temp_predictor = temp_predictor
-        self._temp_requirements_repository = temp_requirements_repository
+        self._weather_forecast_repository = weather_forecast_repository
         self._control_action_repository = control_actions_repository
+        self._control_action_predictor = control_action_predictor
+        self._model_requirements = model_requirements
+        self._timestamp_round_algo = timestamp_round_algo
+        self._timedelta_predict_forward = timedelta_predict_forward
+        self._timedelta = timedelta
+        self._executor = executor
 
-        self._logger.debug(f"Temp predictor is {temp_predictor}")
-        self._logger.debug(f"Temp requirements repository is {temp_requirements_repository}")
-        self._logger.debug(f"Control actions repository is {control_actions_repository}")
-
-    def set_temp_requirements_repository(self, temp_requirements_repository: TempRequirementsDBAsyncRepository):
-        self._logger.debug("Set temp requirements repository")
-        self._temp_requirements_repository = temp_requirements_repository
-
-    def set_control_actions_repository(self, control_actions_repository: ControlActionsSimpleRepository):
-        self._logger.debug("Set control actions repository")
-        self._control_action_repository = control_actions_repository
-
-    def set_temp_predictor(self, temp_predictor: CorrTableTempPredictor):
-        logging.debug("Set temp predictor")
-        self._temp_predictor = temp_predictor
-
-    async def predict_control_actions_async(self):
+    async def update_control_actions_async(self) -> None:
         self._logger.debug("Requested updating control actions")
+        control_action_start_timestamp, control_action_end_timestamp = self._calc_control_action_start_end_timestamp()
+        control_action_current_timestamp = control_action_start_timestamp
+        while control_action_current_timestamp <= control_action_end_timestamp:
+            await self._update_control_action_for_timestamp(control_action_current_timestamp)
+            control_action_current_timestamp += self._timedelta
+        await self._control_action_repository.drop_control_actions_older_than(control_action_start_timestamp)
 
-        async with self._service_lock:
-            temp_requirements_df = await self._get_temp_requirements()
-            control_action_df = await self._calc_control_actions_in_executor(temp_requirements_df)
-            await self._control_action_repository.set_control_action(control_action_df)
-            await self._drop_expired_control_actions()
+    def _calc_control_action_start_end_timestamp(self) -> Tuple[pd.Timestamp, pd.Timestamp]:
+        control_action_start_timestamp = pd.Timestamp.now(tzlocal())
+        control_action_start_timestamp = self._timestamp_round_algo.round_value(control_action_start_timestamp)
+        control_action_start_timestamp = control_action_start_timestamp + self._timedelta
+        control_action_end_timestamp = control_action_start_timestamp + self._timedelta_predict_forward
+        return control_action_start_timestamp, control_action_end_timestamp
 
-    async def _get_temp_requirements(self):
-        start_datetime = pd.Timestamp.now(tz=tzlocal())
-        self._logger.debug(f"Requesting temp requirements from {start_datetime}")
-
-        temp_requirements_df: pd.DataFrame = \
-            await self._temp_requirements_repository.get_temp_requirements(start_datetime)
-        self._logger.debug(f"Gathered {len(temp_requirements_df)} temp requirements")
-
-        return temp_requirements_df
-
-    async def _calc_control_actions_in_executor(self, temp_requirements_df):
-        loop = asyncio.get_running_loop()
-        control_actions_list = await loop.run_in_executor(
-            None,
-            self._calc_control_actions,
-            temp_requirements_df
+    async def _update_control_action_for_timestamp(self,
+                                                   control_action_timestamp: pd.Timestamp
+                                                   ) -> None:
+        weather_forecast_start_timestamp, weather_forecast_end_timestamp = \
+            self._model_requirements.get_weather_start_end_timestamps(control_action_timestamp)
+        weather_forecast_df = await self._weather_forecast_repository.get_weather_forecast_by_timestamp_range(
+            weather_forecast_start_timestamp,
+            weather_forecast_end_timestamp
         )
+        system_states_history_df = dataset_prototypes.HEATING_SYSTEM_STATE.copy()
+        control_action_df = await self._predict_control_action_in_executor(
+            control_action_timestamp,
+            system_states_history_df,
+            weather_forecast_df
+        )
+        await self._control_action_repository.add_control_actions(control_action_df)
 
-        return control_actions_list
-
-    def _calc_control_actions(self, temp_requirements_df):
-        self._logger.debug("Predicting control actions on temp requirements")
-        required_temp_arr = temp_requirements_df[column_names.FORWARD_PIPE_COOLANT_TEMP].to_numpy()
-        control_temp_list = self._temp_predictor.predict_on_temp_requirements(required_temp_arr)
-        control_actions_count = len(control_temp_list)
-        self._logger.debug(f"Calculated {control_actions_count} actions")
-
-        temp_requirements_dates_list = temp_requirements_df[column_names.TIMESTAMP].to_list()
-        control_actions_dates_list = temp_requirements_dates_list[:control_actions_count]
-        control_action_df = pd.DataFrame({
-            column_names.TIMESTAMP: control_actions_dates_list,
-            column_names.FORWARD_PIPE_COOLANT_TEMP: control_temp_list
-        })
-
+    async def _predict_control_action_in_executor(self,
+                                                  control_action_timestamp: pd.Timestamp,
+                                                  system_states_history_df: pd.DataFrame,
+                                                  weather_forecast_df: pd.DataFrame
+                                                  ) -> pd.DataFrame:
+        control_action_df = await asyncio.get_running_loop().run_in_executor(
+            self._executor,
+            self._control_action_predictor.predict_one,
+            weather_forecast_df,
+            system_states_history_df,
+            control_action_timestamp
+        )
         return control_action_df
-
-    async def _drop_expired_control_actions(self):
-        datetime_now = pd.Timestamp.now(tz=tzlocal())
-        self._logger.debug(f"Dropping expired control actions, that older than {datetime_now}")
-        await self._control_action_repository.delete_control_action_older_than(datetime_now)
