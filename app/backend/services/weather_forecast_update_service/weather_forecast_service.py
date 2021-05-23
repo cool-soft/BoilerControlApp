d4109fe961a7306eb93636d90aba56a5e9a539c2
Georgij <netbay.georgij@gmail.com>
2021-05-23 17:25:25 +0500

    ~~~

--- app/backend/services/weather_forecast_update_service/weather_forecast_service.py
+++ app/backend/services/weather_forecast_update_service/weather_forecast_service.py
@@ -1,18 +1,42 @@
+import asyncio
+from concurrent.futures.thread import ThreadPoolExecutor
+
+import pandas as pd
 from boiler.weather.io.abstract_async_weather_loader import AbstractAsyncWeatherLoader
+from boiler.weather.processing import AbstractWeatherProcessor
+from dateutil.tz import UTC
 
 from backend.repositories.weather_forecast_repository import WeatherForecastRepository
 
 
-# TODO: Добавить препроцессор
 class SimpleWeatherForecastService:
 
     def __init__(self,
                  weather_forecast_loader: AbstractAsyncWeatherLoader,
-                 weather_forecast_repository: WeatherForecastRepository
+                 weather_forecast_processor: AbstractWeatherProcessor,
+                 weather_forecast_repository: WeatherForecastRepository,
+                 preload_timedelta: pd.Timedelta = pd.Timedelta(hours=3),
+                 executor: ThreadPoolExecutor = None
                  ) -> None:
         self._weather_forecast_loader = weather_forecast_loader
+        self._weather_forecast_processor = weather_forecast_processor
         self._weather_forecast_repository = weather_forecast_repository
+        self._preload_timedelta = preload_timedelta
+        self._executor = executor
 
     async def update_weather_forecast_async(self) -> None:
-        weather_forecast = await self._weather_forecast_loader.load_weather()
-        await self._weather_forecast_repository.set_weather(weather_forecast)
+        weather_forecast_df = await self._weather_forecast_loader.load_weather()
+        weather_forecast_df = await self._process_weather_forecast_in_executor(weather_forecast_df)
+        await self._weather_forecast_repository.set_weather_forecast(weather_forecast_df)
+
+    async def _process_weather_forecast_in_executor(self, weather_forecast_df: pd.DataFrame) -> pd.DataFrame:
+        start_timestamp = pd.Timestamp.now(tz=UTC)
+        end_timestamp = start_timestamp + self._preload_timedelta
+        preprocessed_weather_forecast_df = await asyncio.get_running_loop().run_in_executor(
+            self._executor,
+            self._weather_forecast_processor.process_weather_df,
+            weather_forecast_df,
+            start_timestamp,
+            end_timestamp
+        )
+        return preprocessed_weather_forecast_df
