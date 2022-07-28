30e1855c149e7f2b5297733d6d12305077b38123
Georgij <netbay.georgij@gmail.com>
2022-07-28 23:57:22 +0500

    ~~~

--- app/backend/services/weather_forecast_update_service.py
+++ app/backend/services/weather_forecast_update_service.py
@@ -1,10 +1,11 @@
+from typing import Callable
+
 import pandas as pd
-from boiler.constants import column_names
 from boiler.weather.io.abstract_sync_weather_loader import AbstractSyncWeatherLoader
 from boiler.weather.processing import AbstractWeatherProcessor
 from dateutil.tz import UTC
+from sqlalchemy.orm import Session
 
-from backend.logging import logger
 from backend.repositories.weather_forecast_repository import WeatherForecastRepository
 
 
@@ -14,31 +15,24 @@ class WeatherForecastUpdateService:
                  weather_forecast_loader: AbstractSyncWeatherLoader,
                  weather_forecast_processor: AbstractWeatherProcessor,
                  weather_forecast_repository: WeatherForecastRepository,
-                 preload_timedelta: pd.Timedelta = pd.Timedelta(hours=3),
+                 session_provider: Callable[..., Session],
+                 preload_timedelta: pd.Timedelta = pd.Timedelta(hours=3)
                  ) -> None:
-        logger.debug("Creating instance")
+        self._session_provider = session_provider
         self._weather_forecast_loader = weather_forecast_loader
         self._weather_forecast_processor = weather_forecast_processor
         self._weather_forecast_repository = weather_forecast_repository
         self._preload_timedelta = preload_timedelta
 
     def update_weather_forecast(self) -> None:
-        logger.info("Requesting weather forecast update")
-
-        weather_forecast_df = self._weather_forecast_loader.load_weather()
-        weather_forecast_df = self._process_weather_forecast(
-            weather_forecast_df
-        )
-        self._weather_forecast_repository.set_weather_forecast(weather_forecast_df)
-
-    def _process_weather_forecast(self, weather_forecast_df: pd.DataFrame) -> pd.DataFrame:
         start_timestamp = pd.Timestamp.now(tz=UTC)
         end_timestamp = start_timestamp + self._preload_timedelta
-        weather_forecast_df = weather_forecast_df.copy()
-        weather_forecast_df[column_names.TIMESTAMP] = weather_forecast_df[column_names.TIMESTAMP].dt.tz_convert(UTC)
-        self._weather_forecast_processor.process_weather_df(
+        weather_forecast_df = self._weather_forecast_loader.load_weather(start_timestamp, end_timestamp)
+        weather_forecast_df = self._weather_forecast_processor.process_weather_df(
             weather_forecast_df,
             start_timestamp,
             end_timestamp
         )
-        return weather_forecast_df
+        with self._session_provider().begin() as session:
+            self._weather_forecast_repository.add_weather_forecast(weather_forecast_df)
+            session.commit()
