7a850b78d1c6e9c9c345e9e00bf28214d57919cc
Georgij <74795914+user11336@users.noreply.github.com>
2022-08-13 13:28:53 +0500

    ~~~

--- app/backend/services/control_action_prediction_service.py
+++ app/backend/services/control_action_prediction_service.py
@@ -5,75 +5,66 @@ import pandas as pd
 from boiler.control_action.predictors.abstract_control_action_predictor import AbstractControlActionPredictor
 from boiler.data_processing.timestamp_round_algorithm import AbstractTimestampRoundAlgorithm
 from boiler.heating_system.model_requirements.abstract_model_requirements import AbstractModelRequirements
-from boiler.temp_requirements.calculators.abstract_temp_requirements_calculator \
-    import AbstractTempRequirementsCalculator
 from dateutil import tz
 from sqlalchemy.orm import scoped_session
 
+from backend.providers.temp_requirements_provider import TempRequirementsProvider
 from backend.repositories.control_action_repository import ControlActionRepository
-from backend.repositories.weather_forecast_repository import WeatherForecastRepository
 
 
 class ControlActionPredictionService:
 
     def __init__(self,
-                 db_session_factory: scoped_session,
-                 weather_forecast_repository: WeatherForecastRepository,
                  model_requirements: AbstractModelRequirements,
-                 temp_requirements_calculator: AbstractTempRequirementsCalculator,
+                 temp_requirements_provider: TempRequirementsProvider,
                  control_action_predictor: AbstractControlActionPredictor,
+                 db_session_factory: scoped_session,
                  control_action_repository: ControlActionRepository,
                  timestamp_round_algo: AbstractTimestampRoundAlgorithm,
                  time_tick: pd.Timedelta,
                  timedelta_predict_forward: timedelta = timedelta(seconds=3600),
                  ) -> None:
-        self._timestamp_round_algo = timestamp_round_algo
-        self._timedelta_predict_forward = timedelta_predict_forward
-        self._time_tick = time_tick
-        self._session_factory = db_session_factory
-        self._weather_forecast_repository = weather_forecast_repository
         self._model_requirements = model_requirements
-        self._temp_requirements_calculator = temp_requirements_calculator
+        self._temp_requirements_provider = temp_requirements_provider
         self._control_action_predictor = control_action_predictor
+        self._session_factory = db_session_factory
         self._control_action_repository = control_action_repository
+        self._timestamp_round_algo = timestamp_round_algo
+        self._time_tick = time_tick
+        self._timedelta_predict_forward = timedelta_predict_forward
 
     def update_control_actions(self) -> None:
-        control_action_start_timestamp, control_action_end_timestamp = self._calc_control_action_start_end_timestamp()
-        weather_start_timestamp, weather_end_timestamp = self._calc_weather_start_end_timestamp(
+        control_action_start_timestamp = pd.Timestamp.now(tz=tz.UTC)
+        control_action_start_timestamp = self._timestamp_round_algo.round_value(control_action_start_timestamp)
+        control_action_end_timestamp = control_action_start_timestamp + self._timedelta_predict_forward
+
+        requirements_start_timestamp, requirements_end_timestamp = self._calc_temp_requirements_start_end_timestamp(
             control_action_start_timestamp,
             control_action_end_timestamp
         )
-        with self._session_factory():
-            weather_forecast_df = self._weather_forecast_repository.get_weather_forecast(
-                weather_start_timestamp,
-                weather_end_timestamp
-            )
-        temp_requirements_df = self._temp_requirements_calculator.calc_for_weather(weather_forecast_df)
+        temp_requirements_df = self._temp_requirements_provider.get_temp_requirements(
+            requirements_start_timestamp, requirements_end_timestamp
+        )
+
         control_action_df = self._calc_control_action(
             control_action_start_timestamp,
             control_action_end_timestamp,
-            temp_requirements_df)
+            temp_requirements_df
+        )
         with self._session_factory() as session:
             self._control_action_repository.add_control_action(control_action_df)
             session.commit()
         self._session_factory.remove()
 
-    def _calc_control_action_start_end_timestamp(self) -> Tuple[pd.Timestamp, pd.Timestamp]:
-        control_action_start_timestamp = pd.Timestamp.now(tz.UTC)
-        control_action_start_timestamp = self._timestamp_round_algo.round_value(control_action_start_timestamp)
-        control_action_start_timestamp = control_action_start_timestamp
-        control_action_end_timestamp = control_action_start_timestamp + self._timedelta_predict_forward
-        return control_action_start_timestamp, control_action_end_timestamp
-
-    def _calc_weather_start_end_timestamp(self,
-                                          control_action_start_timestamp: pd.Timestamp,
-                                          control_action_end_timestamp: pd.Timestamp
-                                          ) -> Tuple[pd.Timestamp, pd.Timestamp]:
-        weather_forecast_start_timestamp, _ = \
+    def _calc_temp_requirements_start_end_timestamp(self,
+                                                    control_action_start_timestamp: pd.Timestamp,
+                                                    control_action_end_timestamp: pd.Timestamp
+                                                    ) -> Tuple[pd.Timestamp, pd.Timestamp]:
+        requirements_start_timestamp, _ = \
             self._model_requirements.get_weather_start_end_timestamps(control_action_start_timestamp)
         _, weather_forecast_end_timestamp = \
             self._model_requirements.get_weather_start_end_timestamps(control_action_end_timestamp)
-        return weather_forecast_start_timestamp, weather_forecast_end_timestamp + self._time_tick
+        return requirements_start_timestamp, weather_forecast_end_timestamp + self._time_tick
 
     def _calc_control_action(self, control_action_start_timestamp, control_action_end_timestamp, temp_requirements_df):
         control_actions_list = []
--- tests/unittests/test_control_action_predict_service_integration.py
+++ tests/unittests/test_control_action_predict_service_integration.py
@@ -1,4 +1,4 @@
-from datetime import timedelta, datetime
+from datetime import datetime
 
 import pandas as pd
 import pytest
@@ -12,29 +12,29 @@ from boiler.heating_system.model_requirements.corr_table_model_requirements impo
 from boiler.temp_requirements.calculators.temp_graph_requirements_calculator import TempGraphRequirementsCalculator
 from boiler_softm_lysva.constants.time_tick import TIME_TICK
 from boiler_softm_lysva.temp_graph.io import SoftMLysvaSyncTempGraphOnlineReader, SoftMLysvaSyncTempGraphOnlineLoader
-from boiler_softm_lysva.weather.io import SoftMLysvaSyncWeatherForecastOnlineReader, \
-    SoftMLysvaSyncWeatherForecastOnlineLoader
+from boiler_softm_lysva.weather.io import \
+    SoftMLysvaSyncWeatherForecastOnlineLoader, \
+    SoftMLysvaSyncWeatherForecastOnlineReader
 from boiler_softm_lysva.weather.processing import SoftMLysvaWeatherForecastProcessor
 from dateutil import tz
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from backend.models.db import ControlAction, WeatherForecast
+from backend.models.db import ControlAction
+from backend.providers.temp_requirements_provider import TempRequirementsProvider
+from backend.providers.weather_forecast_provider import WeatherForecastProvider
 from backend.repositories.control_action_repository import ControlActionRepository
-from backend.repositories.weather_forecast_repository import WeatherForecastRepository
 from backend.services.control_action_prediction_service import ControlActionPredictionService
-from backend.providers.weather_forecast_provider import WeatherForecastService
 
 
-class TestControlActionPredictServiceIntegration:
+class TestControlActionServiceIntegration:
+    db_url = "sqlite:///:memory:"
+    predict_forward_timedelta = pd.Timedelta(minutes=60)
     time_tick = TIME_TICK
-    weather_forecast_preload_timedelta = timedelta(hours=3)
-    timedelta_predict_forward = timedelta(seconds=3600)
+
     min_boiler_temp = 30
     max_boiler_temp = 90
     boiler_temp_step = 1
-
-    db_url = "sqlite:///:memory:"
 
     timedelta_df = pd.DataFrame([
         {column_names.HEATING_OBJ_ID: "obj_1",
@@ -54,31 +54,10 @@ class TestControlActionPredictServiceIntegration:
             for _, row in self.timedelta_df.iterrows():
                 corr_row[column_names.CORRELATED_BOILER_TEMP] = boiler_temp
                 corr_row[row[column_names.HEATING_OBJ_ID]] = \
-                    boiler_temp - (row[column_names.AVG_TIMEDELTA].total_seconds()/max_timedelta*0.5*boiler_temp)
+                    boiler_temp - (row[column_names.AVG_TIMEDELTA].total_seconds() / max_timedelta * 0.5 * boiler_temp)
             corr_list.append(corr_row)
         df = pd.DataFrame(corr_list)
         return df
-
-    @pytest.fixture
-    def session_factory(self):
-        engine = create_engine(self.db_url)
-        with engine.begin() as conn:
-            ControlAction.metadata.drop_all(conn)
-            WeatherForecast.metadata.drop_all(conn)
-            ControlAction.metadata.create_all(conn)
-            WeatherForecast.metadata.create_all(conn)
-        db_session_maker = sessionmaker(
-            autocommit=False,
-            bind=engine
-        )
-        session_factory = scoped_session(
-            db_session_maker
-        )
-        return session_factory
-
-    @pytest.fixture
-    def weather_forecast_repository(self, session_factory):
-        return WeatherForecastRepository(session_factory)
 
     @pytest.fixture
     def weather_forecast_reader(self):
@@ -102,23 +81,11 @@ class TestControlActionPredictServiceIntegration:
         return SoftMLysvaWeatherForecastProcessor()
 
     @pytest.fixture
-    def weather_forecast_update_service(self,
-                                        session_factory,
-                                        weather_forecast_repository,
-                                        weather_forecast_loader,
-                                        weather_forecast_processor
-                                        ):
-        return WeatherForecastService(
+    def weather_forecast_provider(self, weather_forecast_loader, weather_forecast_processor):
+        return WeatherForecastProvider(
             weather_forecast_loader,
-            weather_forecast_processor,
-            weather_forecast_repository,
-            session_factory,
-            preload_timedelta=self.weather_forecast_preload_timedelta
+            weather_forecast_processor
         )
-
-    @pytest.fixture
-    def control_action_repository(self, session_factory):
-        return ControlActionRepository(session_factory)
 
     @pytest.fixture
     def temp_graph_reader(self):
@@ -131,19 +98,45 @@ class TestControlActionPredictServiceIntegration:
         if is_need_proxy:
             http_proxy = proxy_address
             https_proxy = proxy_address
-        loader = SoftMLysvaSyncTempGraphOnlineLoader(
+        return SoftMLysvaSyncTempGraphOnlineLoader(
             reader=temp_graph_reader,
             http_proxy=http_proxy,
             https_proxy=https_proxy
         )
-        return loader
 
     @pytest.fixture
     def temp_requirements_calculator(self, temp_graph_loader):
+        temp_graph = temp_graph_loader.load_temp_graph()
         return TempGraphRequirementsCalculator(
-            temp_graph=temp_graph_loader.load_temp_graph(),
-            weather_temp_round_algorithm=ArithmeticFloatRoundAlgorithm()
+            temp_graph,
+            ArithmeticFloatRoundAlgorithm(decimals=1)
         )
+
+    @pytest.fixture
+    def temp_requirements_provider(self, temp_requirements_calculator, weather_forecast_provider):
+        return TempRequirementsProvider(
+            weather_forecast_provider,
+            temp_requirements_calculator
+        )
+
+    @pytest.fixture
+    def session_factory(self):
+        engine = create_engine(self.db_url)
+        with engine.begin() as conn:
+            ControlAction.metadata.drop_all(conn)
+            ControlAction.metadata.create_all(conn)
+        db_session_maker = sessionmaker(
+            autocommit=False,
+            bind=engine
+        )
+        session_factory = scoped_session(
+            db_session_maker
+        )
+        return session_factory
+
+    @pytest.fixture
+    def control_action_repository(self, session_factory):
+        return ControlActionRepository(session_factory)
 
     @pytest.fixture
     def model_requirements(self):
@@ -163,6 +156,10 @@ class TestControlActionPredictServiceIntegration:
         return SingleTypeTempDeltaCalculator()
 
     @pytest.fixture
+    def timestamp_round_algo(self):
+        return FloorTimestampRoundAlgorithm(self.time_tick)
+
+    @pytest.fixture
     def control_action_predictor(self, heating_system_model, temp_delta_calculator):
         return SingleCircuitControlActionPredictor(
             heating_system_model,
@@ -170,45 +167,33 @@ class TestControlActionPredictServiceIntegration:
         )
 
     @pytest.fixture
-    def timestamp_round_algo(self):
-        return FloorTimestampRoundAlgorithm(self.time_tick)
-
-    @pytest.fixture
     def control_action_predict_service(self,
-                                       session_factory,
-                                       weather_forecast_repository,
                                        model_requirements,
-                                       temp_requirements_calculator,
+                                       temp_requirements_provider,
                                        control_action_predictor,
+                                       session_factory,
                                        control_action_repository,
-                                       timestamp_round_algo,
+                                       timestamp_round_algo
                                        ):
         return ControlActionPredictionService(
-            session_factory,
-            weather_forecast_repository,
             model_requirements,
-            temp_requirements_calculator,
+            temp_requirements_provider,
             control_action_predictor,
+            session_factory,
             control_action_repository,
             timestamp_round_algo,
             self.time_tick,
-            self.timedelta_predict_forward
+            self.predict_forward_timedelta
         )
 
-    def test_update_weather_and_update_control(self,
-                                               session_factory,
-                                               control_action_repository,
-                                               control_action_predict_service,
-                                               weather_forecast_update_service,
-                                               ):
-        control_action_start_timestamp = datetime.now(tz.UTC)
-        control_action_end_timestamp = control_action_start_timestamp + self.timedelta_predict_forward + self.time_tick
-        weather_forecast_update_service.update_weather_forecast()
+    def test_service(self, control_action_predict_service, session_factory, control_action_repository):
         control_action_predict_service.update_control_actions()
+        control_action_start_timestamp = datetime.now(tz.UTC)
+        control_action_end_timestamp = control_action_start_timestamp + self.predict_forward_timedelta + self.time_tick
         with session_factory():
             control_action_df = control_action_repository.get_control_action(
                 control_action_start_timestamp,
                 control_action_end_timestamp,
                 circuit_types.HEATING
             )
-        assert len(control_action_df) == self.timedelta_predict_forward / self.time_tick
+        assert len(control_action_df) == self.predict_forward_timedelta / self.time_tick
